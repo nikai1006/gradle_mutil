4a26eee6ce0ca29d1deec16ea60b2789d73d91db
nikai <keni1006@163.com>
2018-05-13 15:07:41 +0800

    添加用例

--- manager/src/main/java/cn/net/nikai/gradle/manager/Controller.java
+++ manager/src/main/java/cn/net/nikai/gradle/manager/Controller.java
@@ -2,10 +2,14 @@ package cn.net.nikai.gradle.manager;
 
 import cn.net.nikai.gradle.service.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <pre>
  *     test
  * </pre>
+ *
  * @author <a href="nikai.net.cn">nikai</a>
  * @version V1.0.0 2018/5/11 23:32
  */
@@ -14,5 +18,19 @@ public class Controller {
 
     public static void main(String[] args) {
 
+        List<String> list = new ArrayList<String>(1);
+        list.add("1");
+        list.add("2");
+
+        list.forEach(s -> System.out.println(s));
+        list.sort(String::compareToIgnoreCase);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            
+        }
     }
 }
